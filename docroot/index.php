ead7f08290fb40fbb955aca2494273e14ce1fda4
John Peters <john@ILRs-iMac.local>
2014-07-21 10:11:42 -0400

    adjusts padding, layout and fonts for content div

--- docroot/index.php
+++ docroot/index.php
@@ -10,20 +10,31 @@ include("includes/header.php");
   <div id="content" class="region">
     <h1>ILR News Center</h1>
     <h2>"Alive and Well"</h2>
-    <h3>Collective bargaining did its jop in railroad dispute, Margolies says</h3>
+    <h3>Collective bargaining did its job in railroad dispute, Margolies says</h3>
     <p>The Long Island Rail Road schedule remains on track, thanks to compromise borne of the collective bargaining process, according to Worker Institute Senior Associate Ken Margolies.</p>
     <p>“It led to an agreement that recognized the value and needs of the workers, while considering the MTA’s need to find a way to pay for the terms of the pending contract,” he said.</p>
     <p>“That’s what collective bargaining is designed to do and, traditionally, it has produced many more settlements than strikes,” Margolies said.</p>
     <p>The Metropolitan Transportation Authority and an eight-union coalition representing 5,400 railroad workers settled a four-year contract dispute Thursday.</p>
     <p>“At a time when collective bargaining is under attack by politicians and corporations around the world, New Yorkers should take pride that the collective bargaining process is alive and well in the case of the Long Island Rail Road negotiations.”</p>
     <p>Terms announced this week followed fears that a strike would shut down the railroad that transports up to 300,000 riders daily to and from the metro New York area.</p>
-    <p>“The proven methods and techniques of collective bargaining -- including the pressure of a deadline with dire consequences, recommendations from neutrals such as the Presidential Emergency Boards, and skilled mediation from political leadership and negotiators on both sides -- produced a result far better than if there had been a strike or if one side had dictated terms to the other,” Margolies said.</p>
+    <p>“The proven methods and techniques of collective bargaining - including the pressure of a deadline with dire consequences, recommendations from neutrals such as the Presidential Emergency Boards, and skilled mediation from political leadership and negotiators on both sides - produced a result far better than if there had been a strike or if one side had dictated terms to the other,” Margolies said.</p>
     <p>“Speaking as someone who teaches collective bargaining, the press conference was fascinating in how it illustrated the process,” he said, crediting Gov. Andrew Cuomo for steering tension into a productive settlement.</p>
     <p>The contract becomes final when Long Island Railroad unions vote on it; members are expected to vote in favor of the new pact.</p>
   </div>
 
   <div id="sidebar_first" class="region">
     <p>sidebar_first</p>
+    <ul>
+      <li>News nav 1</li>
+      <li>News nav 2</li>
+      <li>News nav 3</li>
+      <li>News nav 4</li>
+      <li>News nav 5</li>
+      <li>News nav 6</li>
+      <li>News nav 7</li>
+      <li>News nav 8</li>
+      <li>News nav 9</li>
+    </ul>
   </div>
 
   <div id="sidebar_second" class="region">
