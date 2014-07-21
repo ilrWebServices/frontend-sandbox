8eec128e5acda204a95820ee2fe36122b190426d
John Peters <john@ILRs-iMac.local>
2014-07-21 13:09:39 -0400

    adds a top nav and footer

--- docroot/index.php
+++ docroot/index.php
@@ -3,18 +3,23 @@ include("includes/header.php");
 ?>
 
 <div class="wrapper">
+
+  <nav>
+    <p>navigation</p>
+  </nav>
+
   <div id="featured" class="region">
     <p>highlighted</p>
   </div>
 
   <div id="content" class="region">
     <h1>ILR News Center</h1>
-    <h2>"Alive and Well"</h2>
+    <h2>“Alive and Well”</h2>
     <h3>Collective bargaining did its job in railroad dispute, Margolies says</h3>
     <p>The Long Island Rail Road schedule remains on track, thanks to compromise borne of the collective bargaining process, according to Worker Institute Senior Associate Ken Margolies.</p>
     <p>“It led to an agreement that recognized the value and needs of the workers, while considering the MTA’s need to find a way to pay for the terms of the pending contract,” he said.</p>
     <p>“That’s what collective bargaining is designed to do and, traditionally, it has produced many more settlements than strikes,” Margolies said.</p>
-    <p>The Metropolitan Transportation Authority and an eight-union coalition representing 5,400 railroad workers settled a four-year contract dispute Thursday.</p>
+    <p>The <a href="">Metropolitan Transportation Authority</a> and an eight-union coalition representing 5,400 railroad workers settled a four-year contract dispute Thursday.</p>
     <p>“At a time when collective bargaining is under attack by politicians and corporations around the world, New Yorkers should take pride that the collective bargaining process is alive and well in the case of the Long Island Rail Road negotiations.”</p>
     <p>Terms announced this week followed fears that a strike would shut down the railroad that transports up to 300,000 riders daily to and from the metro New York area.</p>
     <p>“The proven methods and techniques of collective bargaining - including the pressure of a deadline with dire consequences, recommendations from neutrals such as the Presidential Emergency Boards, and skilled mediation from political leadership and negotiators on both sides - produced a result far better than if there had been a strike or if one side had dictated terms to the other,” Margolies said.</p>
@@ -57,6 +62,11 @@ include("includes/header.php");
       <img src="images/student.jpg" />
     </div><!-- end profile -->
   </div>
+
+  <footer class="region">
+    <p>footer</p>
+  </footer>
+
 </div>
 
 <?php
