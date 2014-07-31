f97b285d3064351aa3ded5b8f9693c8e16485bdb
John Peters <john@ILRs-iMac.local>
2014-07-31 14:42:21 -0400

    adds transparent CU banner to css carousel

---
+++ docroot/index-nav-carousel-trans.php
@@ -0,0 +1,148 @@
+<?php
+include("includes/headerCarouselTrans.php");
+?>
+
+<div class="wrapper">
+
+  <div id="featured-carousel-trans">
+    <div class="navigation">
+      <div id="bannerAdj" class="bannerWhite">
+        <a href="http://www.cornell.edu"></a>
+      </div>
+      <div class="nav navLight">
+        <ul>
+        <li>About ILR</li>
+        <li>Admissions</li>
+        <li>Academics</li>
+        <li>Career Services</li>
+        <li>Professional Programs</li>
+        <li>Centers &amp; Institutes</li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="gallery items-3">
+      <div id="item-1" class="control-operator"></div>
+      <div id="item-2" class="control-operator"></div>
+      <div id="item-3" class="control-operator"></div>
+
+      <figure class="item">
+        <div class="imageWrapper">
+          <img src="images/cornell-1280.jpg" alt="photo" title="photo" />
+        </div>
+      </figure>
+
+      <figure class="item">
+        <div class="imageWrapper">
+          <img src="images/group-1280.jpg" alt="photo" title="photo" />
+        </div>
+      </figure>
+
+      <figure class="item">
+        <div class="imageWrapper">
+          <img src="images/student-1280.jpg" alt="photo" title="photo" />
+        </div>
+      </figure>
+
+      <div class="controls">
+        <a href="#item-1" class="control-button">•</a>
+        <a href="#item-2" class="control-button">•</a>
+        <a href="#item-3" class="control-button">•</a>
+      </div>
+    </div>
+  </div>
+
+  <div id="content" class="region">
+    <h1>ILR News Center</h1>
+    <h2>“Alive and Well”</h2>
+    <h3>Collective bargaining did its job in railroad dispute, Margolies says</h3>
+    <p>The Long Island Rail Road schedule remains on track, thanks to compromise borne of the collective bargaining process, according to Worker Institute Senior Associate Ken Margolies.</p>
+    <p>“It led to an agreement that recognized the value and needs of the workers, while considering the MTA’s need to find a way to pay for the terms of the pending contract,” he said.</p>
+    <p>“That’s what collective bargaining is designed to do and, traditionally, it has produced many more settlements than strikes,” Margolies said.</p>
+    <p>The <a href="">Metropolitan Transportation Authority</a> and an eight-union coalition representing 5,400 railroad workers settled a four-year contract dispute Thursday.</p>
+    <p>“At a time when collective bargaining is under attack by politicians and corporations around the world, New Yorkers should take pride that the collective bargaining process is alive and well in the case of the Long Island Rail Road negotiations.”</p>
+    <p>Terms announced this week followed fears that a strike would shut down the railroad that transports up to 300,000 riders daily to and from the metro New York area.</p>
+    <p>“The proven methods and techniques of collective bargaining - including the pressure of a deadline with dire consequences, recommendations from neutrals such as the Presidential Emergency Boards, and skilled mediation from political leadership and negotiators on both sides - produced a result far better than if there had been a strike or if one side had dictated terms to the other,” Margolies said.</p>
+    <p>“Speaking as someone who teaches collective bargaining, the press conference was fascinating in how it illustrated the process,” he said, crediting Gov. Andrew Cuomo for steering tension into a productive settlement.</p>
+    <p>The contract becomes final when Long Island Railroad unions vote on it; members are expected to vote in favor of the new pact.</p>
+  </div>
+
+  <div id="sidebar_first" class="region">
+    <!-- <p>sidebar_first</p> -->
+    <ul class="menu">
+      <li><a href="">News nav 1</a></li>
+      <li><a href="">News nav 2</a></li>
+      <li><a href="">News nav 3</a></li>
+      <li><a href="">News nav 4</a></li>
+      <li><a class="active" href="">News nav 5</a></li>
+      <li><a href="">News nav 6</a></li>
+      <li><a href="">News nav 7</a></li>
+      <li><a href="">News nav 8</a></li>
+      <li><a href="">News nav 9</a></li>
+    </ul>
+  </div>
+
+  <div id="sidebar_second" class="region">
+    <p>sidebar_second</p>
+    <p>ILR News Center</p>
+    <p>ILR Events</p>
+    <p>Cornell News &amp; Events</p>
+  </div>
+
+  <div id="content_bottom" class="region" data-eq-pts="small: 480, medium: 769, large: 1025">
+    <article role="article" id="node-6816" class="node node-news-item node-teaser contextual-links-region clearfix" data-eq-pts="small: 100, medium: 300, large: 600, full: 900">
+      <h2><a href="/news/no-laughing-matter">No Laughing Matter</a></h2>
+
+      <div class="content">
+        <div class="field field-name-field-published-date field-type-datetime field-label-hidden">
+          <div class="field-items">
+            <div class="field-item even">
+              <!-- <span class="date-display-single">Tuesday, July, 7 2014</span> -->
+              <span class="date-display-single">7.8.14</span>
+            </div>
+          </div>
+        </div>
+
+        <div class="field field-name-field-image field-type-image field-label-hidden">
+          <div class="field-items">
+            <div class="field-item even">
+              <a href="/news/no-laughing-matter">
+                <img src="images/MicheleWilliams-100x100.jpg" width="100" height="100" alt="Workplace culture, training can soften failed humor" title="Workplace culture, training can soften failed humor" />
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div class="field field-name-field-description field-type-text field-label-hidden">
+          <div class="field-items">
+            <div class="field-item even">
+              Workplace culture, training can soften failed humor: research by Williams
+            </div>
+          </div>
+        </div>
+      </div>
+    </article>
+  </div> <!-- end content_bottom -->
+
+  <!-- <div id="content_bottom" class="region layout4x8">
+    <div class="eventSlug">
+      <p class="date"><em>5</em><span>/</span>23 - 25</p>
+      <h2>2014 Commencement Weekend</h2>
+      <p>The 146th Commencement Ceremony will be held on Sunday, May 25, 2014 at Schoellkopf Stadium</p>
+    </div>
+    <div class="profile">
+      <p class="name">Sally Student &lsquo;14</p>
+      <p>Why I chose the MILR</p>
+      <img src="images/student.jpg" />
+    </div>
+  </div> -->
+
+  <footer class="region">
+    <p>footer</p>
+  </footer>
+
+</div>
+
+<?php
+include("includes/footer.php");
+?>
