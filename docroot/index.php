82ac492fc9765952eab711b7eecaf45ec68aca14
John Peters <john@ILRs-iMac.local>
2014-07-31 15:10:11 -0400

    initial commit

--- docroot/index.php
+++ docroot/index.php
@@ -1,26 +1,72 @@
-<?php 
+<?php
 include("includes/header.php");
 ?>
+
 <div class="wrapper">
-  <div class="card">
-    <img src="images/picture.jpg" alt="photo" />
-    <h1>Baseball Card Heading</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+
+  <div id="featured-noBanner" class="region">
+      <div id="bannerAdj" class="bannerGrey">
+        <a href="http://www.cornell.edu"></a>
+      </div>
+      <div class="navColor navDark">
+        <ul>
+        <li>About ILR</li>
+        <li>Admissions</li>
+        <li>Academics</li>
+        <li>Career Services</li>
+        <li>Professional Programs</li>
+        <li>Centers &amp; Institutes</li>
+        </ul>
+      </div>
   </div>
-  <div class="rolodex">
-    <img src="images/picture.jpg" alt="photo" />
-    <h1>Rolodex Heading</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+
+  <div id="content" class="region">
+    <h1>ILR News Center</h1>
+    <img src="images/student.jpg" alt="image" title="image" />
+    <h2>“Alive and Well”</h2>
+    <h3>Collective bargaining did its job in railroad dispute, Margolies says</h3>
+    <p>The Long Island Rail Road schedule remains on track, thanks to compromise borne of the collective bargaining process, according to Worker Institute Senior Associate Ken Margolies.</p>
+    <p>“It led to an agreement that recognized the value and needs of the workers, while considering the MTA’s need to find a way to pay for the terms of the pending contract,” he said.</p>
+    <p>“That’s what collective bargaining is designed to do and, traditionally, it has produced many more settlements than strikes,” Margolies said.</p>
+    <p>The <a href="">Metropolitan Transportation Authority</a> and an eight-union coalition representing 5,400 railroad workers settled a four-year contract dispute Thursday.</p>
+    <p>“At a time when collective bargaining is under attack by politicians and corporations around the world, New Yorkers should take pride that the collective bargaining process is alive and well in the case of the Long Island Rail Road negotiations.”</p>
+    <p>Terms announced this week followed fears that a strike would shut down the railroad that transports up to 300,000 riders daily to and from the metro New York area.</p>
+    <p>“The proven methods and techniques of collective bargaining - including the pressure of a deadline with dire consequences, recommendations from neutrals such as the Presidential Emergency Boards, and skilled mediation from political leadership and negotiators on both sides - produced a result far better than if there had been a strike or if one side had dictated terms to the other,” Margolies said.</p>
+    <p>“Speaking as someone who teaches collective bargaining, the press conference was fascinating in how it illustrated the process,” he said, crediting Gov. Andrew Cuomo for steering tension into a productive settlement.</p>
+    <p>The contract becomes final when Long Island Railroad unions vote on it; members are expected to vote in favor of the new pact.</p>
   </div>
-  <div class="thumb">
-    <img src="images/picture.jpg" alt="photo" />
-    <h1>Thumb Heading</h1>
+
+  <div id="sidebar_first" class="region">
+    <ul class="menu">
+      <li><a href="">News nav 1</a></li>
+      <li><a href="">News nav 2</a></li>
+      <li><a href="">News nav 3</a></li>
+      <li><a href="">News nav 4</a></li>
+      <li><a class="active" href="">News nav 5</a></li>
+      <li><a href="">News nav 6</a></li>
+      <li><a href="">News nav 7</a></li>
+      <li><a href="">News nav 8</a></li>
+      <li><a href="">News nav 9</a></li>
+    </ul>
   </div>
-  <div class="slug">
-    <h1>Slug Heading</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+
+  <div id="sidebar_second" class="region">
+    <p>sidebar_second</p>
+    <p>ILR News Center</p>
+    <p>ILR Events</p>
+    <p>Cornell News &amp; Events</p>
   </div>
+
+  <div id="content_bottom" class="region">
+    <p>sidebar_second</p>
+  </div>
+
+  <footer class="region">
+    <p>footer</p>
+  </footer>
+
 </div>
-<?php 
+
+<?php
 include("includes/footer.php");
 ?>
